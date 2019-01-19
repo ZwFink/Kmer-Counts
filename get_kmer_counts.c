4c5a271c54cfcfa6b1e7bc2ab8f3641e965307de
Bob Costas <finkzane@gmail.com>
2019-01-19 12:34:38 -0700

    added main declaration and includes

---
+++ get_kmer_counts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#include "hash_table.h"
+#include "array_list.h"
+#include "protein_oligo_library.h"
+
+int main( int argc, char **argv )
+{
+
+
+    return EXIT_SUCCESS;
+}
